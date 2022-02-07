8d77471990582faca84d46bca7ffc79a0aef78a7
emereis <82482263+emereis@users.noreply.github.com>
2022-02-07 19:14:50 -0300

    Aula 33

---
+++ Aula 32/ConsoleApp1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int horas;
+
+            Console.WriteLine("Informe o valor de X: ");
+            horas = int.Parse(Console.ReadLine());
+            
+
+            if (horas > 4 && horas < 12 )  {
+                Console.WriteLine("Bom dia!");            
+            }
+
+            if (horas > 12 && horas < 18)   {
+                Console.WriteLine("Boa tarde!");
+            }
+
+            if (horas >= 18 || horas < 04)  {
+                Console.WriteLine("Boa noite!");
+            }
+                
+            
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Aula 33/Aula 33/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Globalization;
+namespace Aula_33
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double nota1, nota2, soma;
+
+            Console.WriteLine("Informe o valor da sua nota no primeiro semestre : ");
+            nota1 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture );
+
+
+
+            Console.WriteLine("Informe o valor da sua nota no segundo semestre : ");
+            nota2 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture) ;
+
+            soma = nota1 + nota2;
+            
+
+            if (soma >= 60.00)
+            {
+                Console.WriteLine("Nota Final = " + soma.ToString ("F1", CultureInfo.InvariantCulture) + " Aprovado!"); ;
+            }
+
+            else
+            
+                Console.WriteLine("REPROVADO!");
+            
+                
+
+
+
+        }
+    }
+}
