bdb7aa4595e03f3313f7f2add61a73a01d9e7d13
emereis <82482263+emereis@users.noreply.github.com>
2022-02-13 20:56:17 -0300

    .

---
+++ Aula 38/Aula 38/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Aula_38
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int A, B, C, D;
+
+            string[] vet = Console.ReadLine().Split(' ');
+
+            A = int.Parse(vet[0]);
+            B = int.Parse(vet[1]);
+            C = int.Parse(vet[2]);
+            D = int.Parse(vet[3]);
+
+            switch (B > C && D > A && C + D > A + B && C > 0 && D > 0 && A % 2 == 0)
+            {
+                case true:
+                    Console.WriteLine("Valores aceitos");
+                    break;
+
+                case false:
+                    Console.WriteLine("Valores não aceitos");
+                    break;
+            }    
+
+
+        }
+
+
+
+    }
+}
+
---
+++ URI/1038/URI 1038/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Globalization;
+
+namespace URI_1038
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+            string[] valores = Console.ReadLine().Split(' ');
+            int codigo = int.Parse(valores[0]);
+            int quantidade = int.Parse(valores[1]);
+
+
+            double total;
+            if (codigo == 1)
+            {
+                total = quantidade * 4.00  ;
+            }
+
+
+            else if (codigo == 2)
+            {
+                total = quantidade * 4.50 ;
+            }
+
+
+            else if (codigo == 3)
+            {
+                total = quantidade * 5.00;
+            }
+
+            else if (codigo == 4)
+            {
+                total = quantidade * 2.00 ;
+            }
+
+            else
+            {
+                total = quantidade * 1.50;
+            }
+                
+            Console.WriteLine("Total: R$ " + total.ToString("F2", CultureInfo.InvariantCulture));
+
+        
+        }
+    }
+}
