a04129f0d55ff9a47a5d14f7fb6b90e48d214192
emereis <82482263+emereis@users.noreply.github.com>
2022-03-21 21:09:16 -0300

    Exercícios URI

---
+++ URI/1113/1113/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace _1113
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string[] valores = Console.ReadLine().Split(' ');
+
+            int x = int.Parse(valores[0]);
+            int y = int.Parse(valores[1]);
+
+
+            while (x != y)
+            {
+                if (x < y)
+                {
+                    Console.WriteLine("Crescente");
+                }
+                else
+                {
+                    Console.WriteLine("Decrescente");
+                }
+
+                valores = Console.ReadLine().Split(' ');
+                x = int.Parse(valores[0]);
+                y = int.Parse(valores[1]);
+
+
+            }
+
+
+
+        }
+    }
+}
