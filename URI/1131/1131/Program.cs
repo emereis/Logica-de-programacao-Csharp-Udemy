c5805af22d89984a42aa481933bb98c5aee989c2
emereis <82482263+emereis@users.noreply.github.com>
2022-03-24 22:16:14 -0300

    ,

---
+++ URI/1131/1131/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+namespace _1131
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int golsinter, golsgremio, jogo, qtdejogos, vitinter, vitgremio, empate;
+            
+
+            qtdejogos = 0;
+            vitgremio = 0;
+            vitinter = 0;
+            empate = 0;
+            jogo = 1;
+
+            while (jogo == 1)
+            {
+                golsinter = int.Parse(Console.ReadLine());
+
+                golsgremio = int.Parse(Console.ReadLine());
+
+                qtdejogos = qtdejogos + 1;
+
+
+                Console.WriteLine("Novo grenal (1-sim 2-nao)");
+                jogo = int.Parse(Console.ReadLine());
+
+                if (golsinter > golsgremio)
+                {
+                    vitinter = vitinter + 1;
+                }
+
+                else if (golsinter < golsgremio)
+                {
+                    vitgremio = vitgremio + 1;
+                }
+
+                else
+                {
+                    empate = empate + 1;
+                }
+
+
+
+            }
+
+            
+
+            Console.WriteLine(qtdejogos + " GRENAIS");
+            Console.WriteLine("Inter:" + vitinter);
+            Console.WriteLine("Gremio:" + vitgremio);
+            Console.WriteLine("Empate:" + empate);
+            
+            if (vitinter > vitgremio)
+            {
+               Console.WriteLine( "Inter venceu mais");
+            }
+
+            else if (vitinter < vitgremio)
+            {
+                Console.WriteLine("Gremio venceu mais");
+            }
+
+            else {
+                Console.WriteLine("Empate");
+
+            }
+        }
+    }
+}
---
+++ URI/1159/1159/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _1159
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int x = int.Parse(Console.ReadLine());
+
+            while (x != 0)
+            {
+
+                if (x % 2 != 0)
+                {
+                    x = x + 1;
+                }
+
+                int soma = x + x + 2 + x + 4 + x + 6 + x + 8;
+                Console.WriteLine(soma);
+
+                x = int.Parse(Console.ReadLine());
+
+            }
+
+
+        }
+    }
+}
